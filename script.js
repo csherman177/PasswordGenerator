7ff52bd881417b9367a8a618c121403f04437daf
Courtney Sherman <csherman177@gmail.com>
2023-03-11 14:50:35 -0700

    new code

---
+++ script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password"); }
+
+ // passwordText.value = password;
+
+ window.prompt ();
+ window.prompt ("How many characters would you like this password to contain?");
+ prompt ("How many characters would you like this password to contain?");
+
+window.confirm ("Do you want to use special characters?")
+
+window.confirm ("Do you want to use uppercase letters?")
+
+window.confirm ("Do you want to use lowercase letters?")
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
