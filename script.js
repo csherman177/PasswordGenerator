4ede3bceb1779d837f4fa29a35c5ee91487ad5d9
Courtney Sherman <csherman177@gmail.com>
2023-03-14 17:55:41 -0600

    new code

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 // Assignment Code
+var specialCharacters = ['!', '@', '#', '%','^','(',')','*', '&', '/','>', '.',';'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var number = ['0','1','2','3','4','5','6','7','8','9'];
+
 var generateBtn = document.querySelector("#generate"); // The variable is holding the button.
 
 // Write password to the #password input
 
 function generatePasswordOptions () {
-var specialCharacters = "!@#%%^()*&/><.,;'[]"
-var upperCase ="ABCDEFGHIJKLMONOPQRSTUVWXYZ"
-var lowerCase = "0123456789abcdefghijklmnopqrstuvwxyz"
+  var options = passwordOptions;
 }
+
+var passwordOptions = {
+  character: character,
+  special: special,
+  upper: upper,
+  lower: lower,
+};
+
+return passwordOptions;
+
 
 function generatePassword() {
   var character = window.prompt("How many characters would you like this password to contain?");
   var password = "";
   var chars = "";
-  }
+  
   if (character < 8) {
     alert("Password length must be at least 8 characters.");
     // return null;
@@ -23,20 +36,24 @@ function generatePassword() {
     alert("Password length cannot exceed 128 characters.")
   }
 
+  //create if statement: if special characters is true the console long (chosen)
   var special = window.confirm("Do you want to use special characters?");
+   if { (special===true)
+    console.log(specialCharacters)    
+    if (special===false)
+    }
   var upper = window.confirm("Do you want to use uppercase letters?");
+  if { (upper===true)
+    console.log(upperCase)    
+    if (special===false)
+    }
   var lower = window.confirm("Do you want to use lowercase letters?");
   var minimumcount = 0;
+  if { (lower===true)
+    console.log(lowerCase)    
+    if (special===false)
+    }
 
-  var passwordOptions = {
-    character: character,
-    special: special,
-    upper: upper,
-    lower: lower,
-  };
-
-  console.log (passwordOptions);
-  // return passwordOptions;
 
   for (var i = 0; i <= character; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
@@ -45,7 +62,9 @@ function generatePassword() {
     console.log(password);
   }
  return password;
+}
 
+var generateBtn = document.querySelector ("#generate");
 
 function writePassword() {
   var password = generatePassword();
@@ -55,22 +74,5 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword () {
-  var options = generatePasswordOptions ();
-  console.log(options);
-
-  if (options.specialCharacters === true) {
-    console.log ('special characters please!',)
-  }
-
-  if (options.upperCase === true) {
-    console.log ('Uppercase characters please!',)
-  }
-
-  if (options.lowerCase === true) {
-    console.log ('Lowercase characters please!',)
-  }
-
-}
 
 
